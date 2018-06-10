a3ca18572edc939d76ce17c52853f4b50c681308
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-06-10 16:47:36 +0500

    clean code

--- src/components/header.js
+++ src/components/header.js
@@ -29,23 +29,3 @@ const styles = {
         color: 'green'
     }
 };
-
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
