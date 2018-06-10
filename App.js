25229cb705e10a64d35cd6a3313befd8b2547e9c
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-06-10 15:55:24 +0500

    clen code

--- App.js
+++ App.js
@@ -25,36 +25,6 @@ export default class App extends Component<Props> {
   render() {
     return (
         <Header />
-      // <View style={styles.container}>
-      //   <Text style={styles.welcome}>
-      //     Welcome to React Native App
-      //   </Text>
-      //   <Text style={styles.instructions}>
-      //     That's first App
-      //   </Text>
-      //   <Text style={styles.instructions}>
-      //     {instructions}
-      //   </Text>
-      // </View>
     );
   }
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
