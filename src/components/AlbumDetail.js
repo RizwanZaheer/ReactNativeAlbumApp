3c31f6ffe00ceeb46908032af3843e1b3a22f08c
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-06-16 16:35:51 +0500

    adding cards & adding card section & adding open url link in the browser etc

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,66 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
 
-class AlbumDetail extends Component {
+export default class AlbumDetail extends Component {
   render() {
-    const { title } = this.props.album;
+    const { title, artist, thumbnail_image, image, url } = this.props.album;
+    const {
+      headerContentStyle, thumbnailStyle, thumbnailContainerStyle, imageStyle, headerTextStyle
+    } = styles;
     return (
-      <View>
-        <Text>{title}</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <View style={thumbnailContainerStyle}>
+            <Image
+              style={thumbnailStyle}
+              source={{ uri: thumbnail_image }}
+            />
+          </View>
+          <View style={headerContentStyle}>
+            <Text style={headerTextStyle}>{title}</Text>
+            <Text>{artist}</Text>
+          </View>
+        </CardSection>
+        <CardSection>
+          <Image
+            style={imageStyle}
+            source={{ uri: image }}
+          />
+        </CardSection>
+        <CardSection>
+          <Button onPress={() => Linking.openURL(url)} >
+            Click me!
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 }
-export default AlbumDetail;
+const styles = {
+  headerContentStyle:
+  {
+    justifyContent: 'space-around',
+    flexDirection: 'column',
+  },
+  headerTextStyle: {
+    fontSize: 18,
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+};
